b742e09770c7001d25eba4eb7ff1774dfcff4c6e
Tom Rune Flo <tom@upnode.no>
2022-05-10 04:26:21 +0200

    move dotenv loading to Init

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -11,16 +11,11 @@ $storagePath = $basePath.'/storage';
 // autoload
 require $basePath.'/vendor/autoload.php';
 
-// load environment file if one exists
-if (file_exists($basePath.'/.env')) {
-    $env = parse_ini_file($basePath.'/.env', true, INI_SCANNER_TYPED);
-    foreach ($env as $key => $value) {
-        $_ENV[$key] = $value;
-    }
-}
-
 // run Init::setup before initializing anything else
 Init::setup();
+
+// load environment file if one exists, keeping existing variables
+Init::loadDotEnv($basePath.'/.env');
 
 // load configuration, create ConfigContainer
 $config = require $basePath.'/config/config.php';
