abdc390e3a0bf5d8f9a370588864aa16849fb8b3
Tom Rune Flo <tom@upnode.no>
2022-06-03 03:55:25 +0200

    moved relay and request creator from bootstrap to inside application

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 use Nanofraim\AbstractProvider;
 use Nanofraim\Application;
 use Nanofraim\Http\ResponseEmitter;
-use Psr\Http\Server\MiddlewareInterface;
-use Relay\Relay;
 use Tomrf\Autowire\Autowire;
 use Tomrf\ConfigContainer\ConfigContainer;
 use Tomrf\DotEnv\DotEnvLoader;
 use Tomrf\ServiceContainer\ServiceContainer;
 use Tomrf\ServiceContainer\ServiceFactory;
-use Tomrf\Session\Session;
 
 // set basePath and storagePath, referenced in configuration files
 $basePath = dirname(__DIR__);
@@ -37,15 +34,10 @@ foreach ($configContainer->search('/phpIni\\..*/') as $key => $value) {
     $phpOptions->set(substr($key, 7), $value);
 }
 
-// create ServiceContainer
+// create ServiceContainer and add services and middleware
 $serviceContainer = new ServiceContainer(new Autowire());
-
-// add services and middleware to ServiceContainer
 $providers = $configContainer->get('providers');
-$classes = array_merge(
-    array_keys($providers),
-    $configContainer->get('middleware')
-);
+$classes = array_merge(array_keys($providers), $configContainer->get('middleware'));
 
 foreach ($classes as $class) {
     if (is_subclass_of($class, AbstractProvider::class)) {
@@ -67,46 +59,10 @@ foreach ($classes as $class) {
 }
 
 // .....
-$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-
-$responseEmitter = new ResponseEmitter();
-
-$serverRequestCreator = new \Nyholm\Psr7Server\ServerRequestCreator(
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory,
-);
-
-$relay = new Relay($configContainer->get('middleware'), function ($class) use ($serviceContainer): MiddlewareInterface {
-    $instance = $serviceContainer->get($class);
-
-    $serviceContainer->fulfillAwarenessTraits(
-        $instance,
-        [
-            'Nanofraim\Trait\ServiceContainerAwareTrait' => [
-                'setServiceContainer' => fn () => $serviceContainer,
-            ],
-            'Psr\Log\LoggerAwareTrait' => [
-                'setLogger' => LoggerInterface::class,
-            ],
-            'Nanofraim\Trait\CacheAwareTrait' => [
-                'setCache' => CacheInterface::class,
-            ],
-            'Nanofraim\Trait\SessionAwareTrait' => [
-                'setSession' => Session::class,
-            ],
-        ]
-    );
-
-    return $instance;
-});
-
 $app = new Application(
     $serviceContainer,
-    $relay,
-    $responseEmitter,
-    $serverRequestCreator,
+    $configContainer,
+    new ResponseEmitter(),
 );
 
 return $app;
--- src/Application.php
+++ src/Application.php
@@ -5,20 +5,26 @@ declare(strict_types=1);
 namespace Nanofraim;
 
 use Nanofraim\Interface\ResponseEmitterInterface;
-use Nyholm\Psr7Server\ServerRequestCreatorInterface;
+use PhpCsFixer\Cache\CacheInterface;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Log\LoggerInterface;
 use Relay\Relay;
 use Tomrf\ServiceContainer\ServiceContainer;
+use Tomrf\Session\Session;
 
 class Application
 {
+    private Relay $relay;
+
     public function __construct(
         protected ServiceContainer $serviceContainer,
-        protected Relay $relay,
+        protected ContainerInterface $configContainer,
         protected ResponseEmitterInterface $responseEmitter,
-        protected ServerRequestCreatorInterface $serverRequestCreator,
     ) {
+        $this->relay = $this->createRelay();
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -33,6 +39,46 @@ class Application
 
     public function createServerRequestFromGlobals(): ServerRequestInterface
     {
+        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+        $this->serverRequestCreator = new \Nyholm\Psr7Server\ServerRequestCreator(
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+        );
+
         return $this->serverRequestCreator->fromGlobals();
+    }
+
+    private function createRelay(): Relay
+    {
+        $serviceContainer = $this->serviceContainer;
+
+        return new Relay(
+            $this->configContainer->get('middleware'),
+            function ($class) use ($serviceContainer): MiddlewareInterface {
+                $instance = $serviceContainer->get($class);
+
+                $serviceContainer->fulfillAwarenessTraits(
+                    $instance,
+                    [
+                        'Nanofraim\Trait\ServiceContainerAwareTrait' => [
+                            'setServiceContainer' => fn () => $serviceContainer,
+                        ],
+                        'Psr\Log\LoggerAwareTrait' => [
+                            'setLogger' => LoggerInterface::class,
+                        ],
+                        'Nanofraim\Trait\CacheAwareTrait' => [
+                            'setCache' => CacheInterface::class,
+                        ],
+                        'Nanofraim\Trait\SessionAwareTrait' => [
+                            'setSession' => Session::class,
+                        ],
+                    ]
+                );
+
+                return $instance;
+            }
+        );
     }
 }
