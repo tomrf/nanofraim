c2fe70a18256147cc9f58a188f5e2c57df12da43
Tom Rune Flo <tom@upnode.no>
2022-09-18 19:10:03 +0200

    remove core lib, split to tomrf/nanofraim-core

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,7 +14,6 @@ return (new PhpCsFixer\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->in(__DIR__.'/src')
-            ->in(__DIR__.'/app')
             ->in(__DIR__.'/bootstrap')
             ->in(__DIR__.'/public')
     )
